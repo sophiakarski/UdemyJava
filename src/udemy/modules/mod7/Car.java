440e1c0236ad205044f5fcd157273af834194d22
sophiakarski <sophiakarski@gmail.com>
2023-09-27 14:44:19 +0100

    Mod7 OOP - Inheritance (Car)

---
+++ src/udemy/modules/mod7/Car.java
@@ -0,0 +1,83 @@
+package udemy.modules.mod7;
+
+public class Car {
+
+//    private fields - defined in class's code block -> defined in class (and not in method)
+//    when create an object in this class, value we assign to these fields represent the state of the object
+//    unlike local variables, class variables should have some type of access modifier declared for it
+//    if don't declare, Java declares default (private) implicitly
+//    REAL application: more fields :)
+//    PRIVATE FIELDS:
+
+//    if assign values, every object that is instantiated gets assigned those, instead of default (null / 0 / false):
+    private String make = "Tesla";
+    private String model = "Model X";
+    private String colour = "Gray";
+    private int doors = 2;
+    private boolean convertible = true;
+
+//    public method (but NOT STATIC)
+//    IntelliJ can 'Generate' code for getters
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    public void setMake(String make) {
+//        this.make = make;
+//        SET VALIDATION RULE:
+        if (make == null) make = "Unknown";
+        String lowercaseMake = make.toLowerCase();
+        switch (lowercaseMake) {
+            case "holden", "porsche", "tesla" -> this.make = make;
+            default -> this.make = "Unsupported ";
+        }
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
+
+    //    method will not be static, as accessing instance fields in the class
+//    method would be public as we want to give way for users to interact with the object
+//    METHOD (TEMPLATE CLASS):
+
+    public void describeCar() {
+        System.out.println(doors + "-Door " +
+                colour + " " +
+                make + " " +
+                model + " " +
+                (convertible ? "Convertible" : ""));
+    }
+
+//    NEXT THING TO DO: create object on this class (as class is just template)
+//    Must create object that takes blueprint (defined in class) and instantiate object to use
+//    GO BACK TO 'Main.java'!!!!
+}
---
+++ src/udemy/modules/mod7/Main.java
@@ -0,0 +1,36 @@
+package udemy.modules.mod7;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car car = new Car();
+//        car.make = "Porsche";
+//        car.setMake("Porsche");
+        car.setMake("Maserati");
+//        ^ MASERATI IS UNSUPPORTED (per validation):
+        car.setModel("Carrera");
+        car.setDoors(2);
+        car.setConvertible(true);
+        car.setColour("black");
+
+//        System.out.println("make = " + car.make);
+//        System.out.println("model = " + car.model);
+        System.out.println("make = " + car.getMake());
+        System.out.println("model = " + car.getModel());
+
+        car.describeCar();
+//        null -> variable / attribute has type, but no reference to object
+
+        Car targa = new Car();
+        targa.setMake("Porsche");
+        targa.setModel("Targa");
+        targa.setDoors(2);
+        targa.setConvertible(false);
+        targa.setColour("red");
+
+        targa.describeCar();
+
+//        :( UGLY ^ duplication of code, and painful to set data one attribute at a time on each object created.
+//        There are better ways to set data on an object!
+    }
+}
