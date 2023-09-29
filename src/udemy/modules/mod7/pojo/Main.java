592a243495d4c1f8d07083dde4161a64d8fdc42b
sophiakarski <sophiakarski@gmail.com>
2023-09-29 15:32:59 +0100

    Add pointDistance Calculator files

--- src/udemy/modules/mod7/pojo/Main.java
+++ src/udemy/modules/mod7/pojo/Main.java
@@ -3,6 +3,7 @@ package udemy.modules.mod7.pojo;
 public class Main {
     public static void main(String[] args) {
 
+//        Wall
         Wall wall = new Wall(5,4);
         System.out.println("Area = " + wall.getArea());
         wall.setHeight(-1.5);
@@ -10,5 +11,16 @@ public class Main {
         System.out.println("Height = " + wall.getHeight());
         System.out.println("Area = " + wall.getArea());
 
+        System.out.println();
+
+//        Point
+        Point first = new Point(6,5);
+        Point second = new Point(3,1);
+        System.out.println("distance(0,0) = " + first.distance());
+        System.out.println("distance(second) = " + first.distance(second));
+        System.out.println("distance(2,2) = " + first.distance(2,2));
+
+        Point point = new Point();
+        System.out.println("distance() = " + point.distance());
     }
 }
