9820c5b77e63fbfbd3a76fe5b6f94e153d204120
sophiakarski <sophiakarski@gmail.com>
2023-09-29 14:53:11 +0100

    Add Area constructor files

---
+++ src/udemy/modules/mod7/pojo/Wall.java
@@ -0,0 +1,34 @@
+package udemy.modules.mod7.pojo;
+
+public class Wall {
+    public double width;
+    public double height;
+
+    public Wall() {
+    }
+
+    public Wall(double width, double height) {
+        this.width = (width < 0) ? 0 : width;
+        this.height = (height < 0) ? 0 :height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = (width < 0) ? 0 : width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = (height < 0) ? 0 : height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+}
