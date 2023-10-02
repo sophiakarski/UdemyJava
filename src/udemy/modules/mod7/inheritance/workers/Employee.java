79d79722e1994cf21ed1bd7f6fab319afa9ecdab
sophiakarski <sophiakarski@gmail.com>
2023-10-02 15:04:02 +0100

    Add Workers Inheritance files

---
+++ src/udemy/modules/mod7/inheritance/workers/Employee.java
@@ -0,0 +1,23 @@
+package udemy.modules.mod7.inheritance.workers;
+
+public class Employee extends Worker {
+// have default constructor for Worker, so Java creates default constructor for Employee
+    private long employeeId;
+    private String hireDate;
+
+    private static int employeeNo = 1;
+
+    public Employee(String name, String birthDate, String hireDate) {
+        super(name, birthDate);
+        this.employeeId = Employee.employeeNo++;
+        this.hireDate = hireDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", hireDate='" + hireDate + '\'' +
+                "} " + super.toString();
+    }
+}
