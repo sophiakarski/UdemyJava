6a4181f89b78c60da08c9709d147d135a35b98c3
sophiakarski <sophiakarski@gmail.com>
2023-10-03 16:19:04 +0100

    Add Workers Inheritance files

--- src/udemy/modules/mod7/inheritance/workers/Main.java
+++ src/udemy/modules/mod7/inheritance/workers/Main.java
@@ -8,7 +8,16 @@ public class Main {
         System.out.println("Age = " + tim.getAge());
         System.out.println("Pay = " + tim.collectPay());
 
-        Employee joe = new Employee("Joe", "11/11/1990", "03/03/2020");
+        SalariedEmployee joe = new SalariedEmployee("Joe", "11/11/1990", "03/03/2020", 35000);
         System.out.println(joe);
+        System.out.println("Joe's paycheck = £" + joe.collectPay());
+
+        joe.retire();
+        System.out.println("Joe's pension check = £" + joe.collectPay());
+
+        HourlyEmployee mary = new HourlyEmployee("Mary", "05/05/1970", "03/03/2021", 15);
+        System.out.println(mary);
+        System.out.println("Mary's paycheck = £" + mary.collectPay());
+        System.out.println("Mary's holiday pay = £" + mary.getDoublePay());
     }
 }
