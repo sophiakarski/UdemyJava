25324ddef58a1e94cec7c052f0a680d3a2a79f84
sophiakarski <sophiakarski@gmail.com>
2023-10-06 15:02:11 +0100

    Add Mod7 files

---
+++ src/udemy/modules/mod7/poolArea/Rectangle.java
@@ -0,0 +1,24 @@
+package udemy.modules.mod7.poolArea;
+
+public class Rectangle {
+
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+        this.width = width < 0 ? 0 : width;
+        this.length = length < 0 ? 0 : length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+}
