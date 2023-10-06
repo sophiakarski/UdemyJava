dcd3b8b06bf654b84436783cb2a19b3dd29ab84d
sophiakarski <sophiakarski@gmail.com>
2023-10-06 15:00:54 +0100

    Add Mod8 Composition files

---
+++ src/udemy/modules/mod8/composition/bedroom/Bed.java
@@ -0,0 +1,41 @@
+package udemy.modules.mod8.composition.bedroom;
+
+public class Bed {
+    private String style;
+    private int pillows;
+    private int height;
+    private int sheets;
+    private int quilt;
+
+    public Bed(String style, int pillows, int height, int sheet, int quilt) {
+        this.style = style;
+        this.pillows = pillows;
+        this.height = height;
+        this.sheets = sheet;
+        this.quilt = quilt;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public int getPillows() {
+        return pillows;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getSheets() {
+        return sheets;
+    }
+
+    public int getQuilt() {
+        return quilt;
+    }
+
+    public void make() {
+        System.out.println("Bed -> Making | ");
+    }
+}
