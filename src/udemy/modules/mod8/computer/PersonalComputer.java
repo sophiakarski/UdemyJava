79d7bec9e35573fd0b840becf192b46c98d5ee9c
sophiakarski <sophiakarski@gmail.com>
2023-10-06 15:23:50 +0100

    Add Computer files

---
+++ src/udemy/modules/mod8/computer/PersonalComputer.java
@@ -0,0 +1,37 @@
+package udemy.modules.mod8.computer;
+
+public class PersonalComputer extends Product {
+    private ComputerCase computerCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+    public PersonalComputer(String model, String manufacturer, ComputerCase computerCase, Monitor monitor, Motherboard motherboard) {
+        super(model, manufacturer);
+        this.computerCase = computerCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    private void drawLogo() {
+        monitor.drawPixelAt(1200, 50, "yellow");
+    }
+
+    public void powerUp() {
+        computerCase.pressPowerButton();
+        drawLogo();
+    }
+
+//    public ComputerCase getComputerCase() {
+//        return computerCase;
+//    }
+//
+//    public Monitor getMonitor() {
+//        return monitor;
+//    }
+//
+//    public Motherboard getMotherboard() {
+//        return motherboard;
+//    }
+
+//    how to access functionality of parts through personal computer container
+}
---
+++ src/udemy/modules/mod8/computer/Product.java
@@ -0,0 +1,72 @@
+package udemy.modules.mod8.computer;
+
+public class Product {
+    private String model;
+    private String manufacturer;
+    private int width;
+    private int height;
+    private int depth;
+
+    public Product(String model, String manufacturer) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+    }
+}
+
+class Monitor extends Product {
+    private int size;
+    private String resolution;
+
+    public Monitor(String model, String manufacturer) {
+        super(model, manufacturer);
+    }
+
+    public Monitor(String model, String manufacturer, int size, String resolution) {
+        super(model, manufacturer);
+        this.size = size;
+        this.resolution = resolution;
+    }
+
+    public void drawPixelAt(int x, int y, String colour) {
+        System.out.println(String.format(
+                "Drawing pixel at %d, %d in colour %s ", x, y, colour));
+    }
+}
+
+class Motherboard extends Product {
+    private int ramSlots;
+    private int cardSlots;
+    private String bios;
+
+    public Motherboard(String model, String manufacturer) {
+        super(model, manufacturer);
+    }
+
+    public Motherboard(String model, String manufacturer, int ramSlots, int cardSlots, String bios) {
+        super(model, manufacturer);
+        this.ramSlots = ramSlots;
+        this.cardSlots = cardSlots;
+        this.bios = bios;
+    }
+
+    public void loadProgramme(String programmeName) {
+        System.out.println("Programme " + programmeName + " is now loading...");
+    }
+}
+
+class ComputerCase extends Product {
+    private String powerSupply;
+
+    public ComputerCase(String model, String manufacturer) {
+        super(model, manufacturer);
+    }
+
+    public ComputerCase(String model, String manufacturer, String powerSupply) {
+        super(model, manufacturer);
+        this.powerSupply = powerSupply;
+    }
+
+    public void pressPowerButton() {
+        System.out.println("Power button pressed");
+    }
+}
